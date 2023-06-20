df02f2a511e5b3576ca187e3b2a45e55e37269be
timka <git@timka.space>
2023-06-20 10:02:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Автоап тем на форумах
+
+## Поддерживаемые форумы
+
+* zelenka.guru
+* cybhack.net
+* endway.su
+
+## Установка скрипта
+
+```bash
+git clone https://github.com/timka-123/forum-autobump
+cd forum-autobump
+pip install -r requirements.txt
+python main.py
+```
+
+## Настройка конфига
+
+Создайте файл `.env` и заполните его по образцу с `.env.example`
+
+Разберу по пунктам, что и как заполнять:
+
+* `tg_token` - токен тг бота, куда будут идти уведомления
+* `tg_user` - ID пользователя, кому будут приходить уведомления о циклах апа
+* `zelenka_token` - ваш OAuth токен Zelenka.guru. Вы можете получить его [здесь](https://zelenka.guru/account/authorize?client_id=1rrspe9aiw&response_type=token&scope=basic+read+post) (потребуется скопировать токен из строки браузера)
+* `endway_cookie` - ваши Cookie файлы Endway
+* `endway_token` - ваш токен EndWay, посмотрите в любом POST запросе под ключом `_xfToken`
+* `cybhack_token` - ваш токен Cybhack, посмотрите в любом POST запросе под ключом `_xfToken`
+* `cybhack_cookie` - ваши Cookie файлы Cybhack
+* `endway_themes` - список ID тем, которые нужно апать через пробел. (https://endway.su/threads/это и есть id темы/)
+* `cybhack_themes` - список ID тем, которые нужно апать через пробел. (https://cybhack.su/threads/это и есть id темы/)
+* `zelenka_themes` - список ID тем, которые нужно апать через пробел. (https://zelenka.guru/threads/это и есть id темы/)
+
---
+++ main.py
@@ -0,0 +1,110 @@
+from asyncio import run
+from os import environ
+from time import sleep
+
+from aiohttp import ClientSession
+from dotenv import load_dotenv
+
+load_dotenv()
+
+tg_token = environ.get("tg_token")
+user = environ.get("tg_user")
+
+endway_themes = environ.get("endway_themes").split()
+cybhack_themes = environ.get("cybhack_themes").split()
+zelenka_themes = environ.get("zelenka_themes").split()
+
+endway_cookie = environ.get("endway_cookie")
+endway_token = environ.get("endway_token")
+
+cybhack_token = environ.get("cybhack_token")
+cybhack_cookie = environ.get("cybhack_cookie")
+
+zelenka_token = environ.get("zelenka_token")
+
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0"
+
+
+async def send_message(message: str):
+    async with ClientSession() as session:
+        data = await session.post(
+            url=f"https://api.telegram.org/bot{tg_token}/sendMessage",
+            data={
+                "chat_id": user,
+                "text": message,
+                "parse_mode": "HTML"
+            }
+        )
+        json = await data.json()
+        print(json)
+
+
+async def main_loop():
+    # endway auto up
+    for theme in endway_themes:
+        async with ClientSession(headers={
+            "Cookie": endway_cookie,
+            "User-Agent": user_agent
+        }) as session:
+            response = await session.post(
+                url="https://endway.su/threads/" + theme + "/bump",
+                data={
+                    "_xfWithData": "1",
+                    "_xfToken": endway_token,
+                    "_xfResponseType": "json",
+                    "_xfRequestUri": "https://endway.su/threads/" + theme
+                }
+            )
+            # todo remake
+            data = await response.json()
+            message = f"""✅ Ап темы https://endway.su/threads/{theme}
+
+⏳ Статус: <b>{data['status']}</b>"""
+            if data['status'] == 'error':
+                message += f"\n💫 Ошибка: <code>{data['errors'][0]}</code>"
+            await send_message(message)
+            await session.close()
+    # cybhack auto up
+    for theme in cybhack_themes:
+        async with ClientSession(headers={
+            "Cookie": cybhack_cookie,
+            "User-Agent": user_agent
+        }) as session:
+            response = await session.post(
+                url="https://cybhack.net/threads/" + theme + "/up",
+                data={
+                    "_xfWithData": "1",
+                    "_xfToken": cybhack_token,
+                    "_xfResponseType": "json",
+                    "_xfRequestUri": "/threads/" + theme
+                }
+            )
+            data = await response.json()
+            message = f"""✅ Ап темы https://cybhack.net/threads/{theme}
+
+⏳ Статус: <b>{data['status']}</b>"""
+            if data['status'] == 'error':
+                message += f"\n💫 Ошибка: <code>{data['errors'][0]}</code>"
+            await send_message(message)
+            await session.close()
+    # zelenka autobump
+    for theme in zelenka_themes:
+        async with ClientSession(headers={"Authorization": f"Bearer {zelenka_token}"}) as session:
+            response = await session.post(
+                url=f"https://api.zelenka.guru/threads/{theme}/bump"
+            )
+            data = await response.json()
+            message = f"""✅ Ап темы https://zelenka.guru/threads/{theme}
+
+⏳ Статус: <b>{"✅ Успешно" if not data.get("errors") else "❌ Ошибка"}</b>"""
+            if data.get("errors"):
+                error = data['errors'][0].replace("<br>", "\n")
+                message += f"\n💫 Ошибка: <code>{error}</code>"
+            await send_message(message)
+            await session.close()
+
+
+if __name__ == "__main__":
+    while True:
+        run(main_loop())
+        sleep(86400)
